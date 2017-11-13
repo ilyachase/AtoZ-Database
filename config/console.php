d245a887b0b1dbe2cb0bfb772bad4a74b9f55beb
ilyachase <LyWehobain7>
2017-11-13 21:21:58 +0500

    more logs

--- config/console.php
+++ config/console.php
@@ -24,7 +24,7 @@ $config = [
 					'logFile'        => '@runtime/logs/trace.log',
 					'levels'         => [ 'trace' ],
 					'logVars'        => [],
-					'exportInterval' => 10,
+					'exportInterval' => 3,
 				],
 			],
 		],
